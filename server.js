4b975996d922a2255ed398a36fc370977fe7cbb8
zazamaruashvili01 <zazamaruashvili01@gmail.com>
2026-02-26 11:39:47 +0400

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.post("/submit", (req, res) => {
   res.send("მონაცემი მიღებულია ✅");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
